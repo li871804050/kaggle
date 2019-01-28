f5d9853f4982d5acd408b503cd0437232df8e8cb
liyonglin <liyonglin@banggood.com>
2019-01-28 18:02:03 +0800

    statsmodel line_regression

--- tianjin/sk_line.py
+++ tianjin/sk_line.py
@@ -4,6 +4,7 @@ from sklearn.metrics import mean_squared_error
 from tianjin import deal_data
 import pandas as pd
 import numpy as np
+import statsmodels.api as sm
 
 clf = linear_model.LinearRegression()
 
@@ -12,7 +13,7 @@ test_datas = np.asarray(deal_data.data('data/jinnan_round1_testA_20181227.csv'))
 ids = pd.read_csv('data/jinnan_round1_testA_20181227.csv', encoding='gbk')['样本id']
 
 y = train_datas[:, -1:]
-y = [x for x in y]
+# y = [x for x in y]
 x = train_datas[:, : -1]
 
 train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.1, random_state=1)
@@ -29,3 +30,14 @@ write = open('data/res_20190128.csv', 'w')
 for i in range(len(pred_test)):
     write.write('%s,%f\r'%(ids[i], pred_test[i]))
 write.close()
+
+
+# model = sm.OLS(train_y, train_x)
+# results = model.fit()
+# print(train_x.shape)
+# print(train_y.shape)
+# pred = model.predict(test_x)
+#
+# # print(test_x.shape, pred.shape)
+# loss = mean_squared_error(test_y, pred)
+# print(loss)
